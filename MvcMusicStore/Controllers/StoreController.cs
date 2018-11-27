cf31645a4f36b633636645336d74496aaa3587e2
Rutulpatel4 <31968482+Rutulpatel4@users.noreply.github.com>
2018-11-27 15:07:42 -0500

    CheckOut and Order

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -37,15 +37,26 @@ namespace MvcMusicStore.Controllers
             string CurrentCartId;
             GetCartId();
             CurrentCartId = Session["CartId"].ToString();
-            //create and save the new cart item
-            Cart cart = new Cart
+
+            //if item is already in the user's cart then icrement by 1
+            Cart cart = db.Carts.SingleOrDefault(c => c.AlbumId == AlbumId && c.CartId == CurrentCartId);
+
+            if(cart == null)
             {
-                CartId = CurrentCartId,
-                AlbumId = AlbumId,
-                Count = 1,
-                DateCreated = DateTime.Now
-            };
-            db.Carts.Add(cart);
+                //create and save the new cart item
+                cart = new Cart
+                {
+                    CartId = CurrentCartId,
+                    AlbumId = AlbumId,
+                    Count = 1,
+                    DateCreated = DateTime.Now
+                };
+                db.Carts.Add(cart);
+            }
+            else
+            {
+                cart.Count++;
+            }
             db.SaveChanges();
 
             //load the shopping cart page
@@ -93,6 +104,84 @@ namespace MvcMusicStore.Controllers
 
             //reload the updated cart page
             return RedirectToAction("ShoppingCart");
+        }
+
+        //GET Store/Checkout
+        [Authorize]
+        public ActionResult Checkout()
+        {
+            //migrate the cart to the username if shopping anonymous
+            MigrateCart();
+            return View();
+        }
+
+        //Post Store/Checkour
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Checkout(FormCollection values)
+        {
+            //create a new order and populate it from value
+            Order order = new Order();
+            TryUpdateModel(order);
+
+            //autopopulate the field that we can 
+            order.Username = User.Identity.Name;
+            order.Email = User.Identity.Name;
+            order.OrderDate = DateTime.Now;
+
+            //get user's item and calc cart total
+            var CartItems = db.Carts.Where(c => c.CartId == User.Identity.Name);
+            decimal OrderTotal = (from c in CartItems
+                                  select (int)c.Count * c.Album.Price).Sum();
+            order.Total = OrderTotal;
+
+            //save main order
+            db.Orders.Add(order);
+            db.SaveChanges();
+
+            //now save each items
+            foreach (Cart item in CartItems)
+            {
+                OrderDetail od = new OrderDetail
+                {
+                    OrderId = order.OrderId,
+                    AlbumId = item.AlbumId,
+                    Quantity = item.Count,
+                    UnitPrice = item.Album.Price
+                };
+                db.OrderDetails.Add(od);
+            }
+
+            //save all the order items
+            db.SaveChanges();
+
+            //show the recipt
+            return RedirectToAction("Details", "Orders" , new { id = order.OrderId });
+        }
+
+        private void MigrateCart()
+        {
+            //attach anonymous cart to an authenticated once they log in
+            if(!String.IsNullOrEmpty(Session["CartId"].ToString()) && User.Identity.IsAuthenticated)
+            {
+                if(Session["CartId"].ToString() != User.Identity.Name)
+                {
+                    //get cart item with random id
+                    string CurrentCartId = Session["CartId"].ToString();
+                    var CartItems = db.Carts.Where(c => c.CartId == CurrentCartId);
+
+                    foreach(Cart item in CartItems)
+                    {
+                        item.CartId = User.Identity.Name;
+                    }
+
+                    db.SaveChanges();
+
+                    //update the session variable to the username
+                    Session["CartId"] = User.Identity.Name;
+                }
+            }
         }
     }
 }
